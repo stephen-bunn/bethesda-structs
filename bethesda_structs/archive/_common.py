eb7b009842101add3fb935eb4725d5f20814e354
Stephen Bunn <stephen@bunn.io>
2018-04-03 12:45:07 -0400

    Better extraction for BSAArchive
    - now includes basic progress hooks

--- bethesda_structs/archive/_common.py
+++ bethesda_structs/archive/_common.py
@@ -1,29 +1,28 @@
 # Copyright (c) 2018 Stephen Bunn <stephen@bunn.io>
 # GPLv3 License <https://choosealicense.com/licenses/gpl-3.0/>
 
-import os
 import io
+import os
 import abc
-
-from typing import (TypeVar, Generic,)
+from typing import Generic, TypeVar, Callable
 
 import attr
-from construct import (Construct, Container, Subconstruct,)
+from construct import Construct, Container, Subconstruct
 
-
-T_BaseArchive = TypeVar('BaseArchive')
+T_BaseArchive = TypeVar("BaseArchive")
 
 
 @attr.s
 class BaseArchive(abc.ABC, Generic[T_BaseArchive]):
     """The base class all Archives should subclass.
     """
-
     content = attr.ib(type=bytes, repr=False)
     filepath = attr.ib(type=str, default=None)
     container = attr.ib(type=Container, default=None, repr=False, init=False)
 
     def __attrs_post_init__(self):
+        """Initializes the non-init attributes.
+        """
         self.container = self.archive_struct.parse(self.content)
 
     @abc.abstractproperty
@@ -33,7 +32,6 @@ class BaseArchive(abc.ABC, Generic[T_BaseArchive]):
         Returns:
             Construct: The archive structure
         """
-
         raise NotImplementedError
 
     @abc.abstractclassmethod
@@ -44,37 +42,94 @@ class BaseArchive(abc.ABC, Generic[T_BaseArchive]):
             filepath (str): The filepath to evaluate
 
         Raises:
-            NotImplementedError: Subclasses must define the logic
+            NotImplementedError: Subclasses must implement
 
         Returns:
             bool: True if the `filepath` can be handled, otherwise False
         """
-
         raise NotImplementedError
 
     @classmethod
-    def parse_bytes(
-        cls,
-        content: bytes,
-        filepath: str=None
-    ) -> T_BaseArchive:
+    def parse_bytes(cls, content: bytes, filepath: str = None) -> T_BaseArchive:
+        """Create a `BaseArchive` from a byte array.
+
+        Args:
+            content (bytes): The byte content of the archive
+            filepath (str, optional): Defaults to None. Sets the filepath attribute for
+                user's reference
+
+        Raises:
+            ValueError: If the given content is not of bytes
+
+        Returns:
+            T_BaseArchive: A created `BaseArchive`
+        """
+        if not isinstance(content, bytes):
+            raise ValueError(
+                f"given content must be of bytes, recieved {type(content)!r}"
+            )
+
         return cls(content, filepath=filepath)
 
     @classmethod
     def parse_stream(
-        cls,
-        stream: io.BufferedReader,
-        filepath: str=None
+        cls, stream: io.BufferedReader, filepath: str = None
     ) -> T_BaseArchive:
+        """Create a `BaseArchive` from a file stream.
+
+        Args:
+            stream (io.BufferedReader): A file stream to read from.
+            filepath (str, optional): Defaults to None. Sets the filepath attribute for
+                user's reference.
+
+        Raises:
+            ValueError: If the given stream is not of ``bytes``
+
+        Returns:
+            T_BaseArchive: A created `BaseArchive`
+        """
+        if not isinstance(stream.peek(1), bytes):
+            raise ValueError(
+                (
+                    f"stream {stream!r} is not a stream of bytes, "
+                    f"recieved {type(stream)!r}"
+                )
+            )
+
         return cls.parse_bytes(stream.read(), filepath=filepath)
 
     @classmethod
     def parse_file(cls, filepath: str) -> T_BaseArchive:
+        """Create a `BaseArchive` from a given filepath.
+
+        Args:
+            filepath (str): The filepath to read from
+
+        Raises:
+            FileNotFoundError: If the given filepath does not exist
+
+        Returns:
+            T_BaseArchive: A created `BaseArchive`
+        """
         if not os.path.isfile(filepath):
             raise FileNotFoundError(f"no such file '{filepath!r}' exists")
-        with open(filepath, 'rb') as stream:
+
+        with open(filepath, "rb") as stream:
             return cls.parse_stream(stream, filepath)
 
     @abc.abstractmethod
-    def extract(self, to_dir: str):
+    def extract(
+        self, to_dir: str, progress_hook: Callable[[int, int, str], None] = None
+    ):
+        """Extracts the content of the `BaseArchive` to the given directory.
+
+        Args:
+            to_dir (str): The directory to extract the content to
+            progress_hook (Callable[[int, int, str], None], optional): Defaults to None.
+                A progress hook that should expect (``current``, ``total``,
+                ``current_filepath``) as arguments
+
+        Raises:
+            NotImplementedError: Subclasses must implement
+        """
         raise NotImplementedError
--- bethesda_structs/archive/bsa.py
+++ bethesda_structs/archive/bsa.py
@@ -2,22 +2,46 @@
 # GPLv3 License <https://choosealicense.com/licenses/gpl-3.0/>
 
 import os
-from pathlib import Path
+from typing import Callable
+from pathlib import Path, PureWindowsPath
 
 from construct import (
-    Probe, Struct, Const, If, IfThenElse,
-    Array, GreedyRange, GreedyBytes, Compressed,
-    PascalString, CString, PaddedString, FlagsEnum,
-    VarInt, Int64ul, Int32ul, Int8ul,
+    If,
+    Array,
+    Bytes,
+    Const,
+    Probe,
+    Int8ul,
+    Struct,
+    VarInt,
+    CString,
+    Int32ul,
+    Int64ul,
+    FlagsEnum,
+    Compressed,
+    IfThenElse,
+    GreedyBytes,
+    GreedyRange,
+    PaddedString,
+    PascalString,
+    EmbeddedSwitch
 )
+
 from ._common import BaseArchive
 
 
 class BSAArchive(BaseArchive):
+    """The archive for BSA file formats.
 
+    Note:
+        This archive does *not* extract the file structures from the given
+        content. This is handled during extraction due to the nature of how the file
+        offsets need to be chosen from the file and directory records with a bit more
+        logic than the base structure should handle.
+    """
     archive_struct = Struct(
         "header" / Struct(
-            "magic" / Const(b'BSA\x00'),
+            "magic" / Const(b"BSA\x00"),
             "version" / Int32ul,
             "offset" / Int32ul,
             "flags" / FlagsEnum(
@@ -31,7 +55,7 @@ class BSAArchive(BaseArchive):
                 xbox360_archive=0x040,
                 _unknown_3=0x100,
                 _unknown_4=0x200,
-                _unknown_5=0x400
+                _unknown_5=0x400,
             ),
             "directory_count" / Int32ul,
             "file_count" / Int32ul,
@@ -47,8 +71,8 @@ class BSAArchive(BaseArchive):
                 shaders=0x020,
                 trees=0x040,
                 fonts=0x080,
-                miscellaneous=0x100
-            )
+                miscellaneous=0x100,
+            ),
         ),
         "directory_records" / Array(
             lambda this: this.header.directory_count,
@@ -58,79 +82,148 @@ class BSAArchive(BaseArchive):
                 "_unknown_0" / Int32ul,
                 "file_offset" / Int32ul,
                 "_unknown_1" / Int32ul,
-            )
+            ),
         ),
         "file_blocks" / Array(
             lambda this: this.header.directory_count,
             Struct(
                 "name" / If(
                     lambda this: this._.header.flags.has_names_for_directories,
-                    PascalString(VarInt, 'utf8'),
+                    PascalString(VarInt, "utf8"),
                 ),
                 "records" / Array(
-                    lambda this: this._.directory_records[
-                        this._._index
-                    ].file_count,
+                    lambda this: this._.directory_records[this._._index].file_count,
                     Struct(
                         "name_hash" / Int64ul,
                         "size" / Int32ul,
                         "offset" / Int32ul
-                    )
-                )
-            )
+                    ),
+                ),
+            ),
         ),
         "file_names" / If(
             lambda this: this.header.flags.has_names_for_files,
-            Array(lambda this: this.header.file_count, CString('utf8'))
-        )
+            Array(
+                lambda this: this.header.file_count,
+                CString("utf8")
+            ),
+        ),
     )
 
     @classmethod
     def can_handle(cls, filepath: str) -> bool:
-        header = cls.header_struct.parse_file(filepath)
-        return header.bsa == b'BSA\x00' and header.version == 105
+        """Determines if a given `filepath` can be handled by the archive.
 
-    def extract(self, to_dir: str):
+        Args:
+            filepath (str): The filepath to evaluate
+
+        Raises:
+            NotImplementedError: Subclasses must implement
+
+        Returns:
+            bool: True if the `filepath` can be handled, otherwise False
+        """
+        header = cls.header_struct.parse_file(filepath)
+        return header.magic == b"BSA\x00" and header.version in (103, 104, 105)
+
+    def extract(self, to_dir: str, progress_hook: Callable[[int, int, str], None]=None):
+        """Extract the content of a BSA archive out to a given directory.
+
+        Note:
+            Progress hooks are very basic and implemented in two stages. This means
+            that you will mostly get duplicate calls with the same ``current``,
+            ``total``, and ``current_filepath``. This shouldn't be much of a problem
+            for your hooks because you should make sure that an update with duplicate
+            data doesn't change the displayed progress.
+
+        Args:
+            to_dir (str): The directory to extract the content into
+            progress_hook (Callable[[int, int, str], None], optional): Defaults to None.
+                A progress hook that should expect (``current``, ``total``,
+                ``current_filepath``) as arguments
+
+        Raises:
+            NotADirectoryError: If the given directory does not exist
+            ValueError: Various file reading errors
+        """
         if not os.path.isdir(to_dir):
             raise NotADirectoryError(f"no such directory '{to_dir!r}' exists")
 
+        # defines the compressed and uncompressed file structure using an EmbeddedSwitch
+        file_struct = EmbeddedSwitch(
+            Struct(
+                "name" / If(
+                    self.container.header.flags.files_compressed,
+                    CString("utf8")
+                )
+            ),
+            self.container.header.flags.files_compressed,
+            {
+                True: Struct(
+                    "original_size" / Int32ul,
+                    "data" / Compressed(GreedyBytes, "zlib")
+                ),
+                False: Struct("data" / GreedyBytes),
+            },
+        )
+
+        # NOTE: could be either compressed or uncompressed size
+        total_size = sum(
+            record.size
+            for block in self.container.file_blocks
+            for record in block.records
+        )
+        current_size = 0
+        file_index = 0
         to_dir = Path(to_dir)
+
+        # iterate over zipped directory records and file blocks
+        # (should always be the same length)
         for (directory_record, file_block) in zip(
-            self.container.directory_records,
-            self.container.file_blocks
+            self.container.directory_records, self.container.file_blocks
         ):
-            directory_fragment = Path(file_block.name[:-1].replace('\\', '/'))
-            for (file_idx, file_record) in enumerate(file_block.records):
-                if self.container.header.flags.files_compressed:
-                    file_struct = Struct(
-                        "name" / If(
-                            self.container.header.flags.files_compressed,
-                            CString('utf8')
-                        ),
-                        "original_size" / Int32ul,
-                        "data" / Compressed(GreedyBytes, 'zlib')
-                    )
+            directory_path = to_dir
+            if isinstance(file_block.name, str):
+                directory_path = directory_path.joinpath(
+                    PureWindowsPath(file_block.name[:-1])
+                )
+
+            # iterate over all files in the file block
+            for file_record in file_block.records:
+                to_path = directory_path
+                file_container = file_struct.parse(
+                    self.content[
+                        file_record.offset:(file_record.offset + file_record.size)
+                    ]
+                )
+                if isinstance(file_container.name, str):
+                    to_path = to_dir.joinpath(PureWindowsPath(file_container.name))
                 else:
-                    file_struct = Struct(
-                        "name" / If(
-                            self.container.header.flags.files_compressed,
-                            CString('utf8')
-                        ),
-                        "data" / GreedyBytes
+                    if len(self.container.file_names) <= 0:
+                        raise ValueError((
+                            f"no available file name for file {file_container!r}, "
+                            f"maybe corrupt file or unsupported bsa version"
+                        ))
+
+                    # archive file names should always be the same length of the
+                    # header's `file_count`
+                    to_path = directory_path.joinpath(
+                        self.container.file_names[file_index]
                     )
+                    file_index += 1
 
-                file_container = file_struct.parse(self.content[
-                    file_record.offset:(file_record.offset + file_record.size)
-                ])
-                if file_container.name is not None:
-                    file_path = Path(file_container.name.replace('\\', '/'))
-                    directory_fragment = file_path.parent
-                    path_fragment = file_path.name
-                if self.container.file_names is not None:
-                    path_fragment = self.container.file_names[file_idx]
+                posix_path = to_path.as_posix()
+                # push pre-stage progress (includes 0.0 for first call)
+                if callable(progress_hook):
+                    progress_hook(current_size, total_size, posix_path)
 
-                with to_dir.joinpath(
-                    directory_fragment,
-                    path_fragment
-                ).open('wb') as stream:
+                # create parent directories and write data out
+                if not to_path.parent.is_dir():
+                    to_path.parent.mkdir(parents=True)
+                with to_path.open("wb") as stream:
                     stream.write(file_container.data)
+                current_size += len(file_container.data)
+
+                # push post-stage progress (includes 100.0 for last call)
+                if callable(progress_hook):
+                    progress_hook(current_size, total_size, posix_path)
