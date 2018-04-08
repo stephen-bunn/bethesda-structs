b4ab3fc7ccb41f7c8eeef2576614d08320323b46
Stephen Bunn <stephen@bunn.io>
2018-04-07 23:20:57 -0400

    Splitting up BSA extraction logic into per-structure namespaces

--- bethesda_structs/archive/__init__.py
+++ bethesda_structs/archive/__init__.py
@@ -1,2 +1,4 @@
 # Copyright (c) 2018 Stephen Bunn <stephen@bunn.io>
 # GPLv3 License <https://choosealicense.com/licenses/gpl-3.0/>
+
+from .bsa import BSAArchive
--- bethesda_structs/archive/_common.py
+++ bethesda_structs/archive/_common.py
@@ -4,14 +4,31 @@
 import io
 import os
 import abc
-from typing import Generic, TypeVar, Callable
+from typing import Generic, TypeVar, Callable, Generator
+from pathlib import Path
 
 import attr
 from construct import Construct, Container, Subconstruct
 
 from .._common import BaseFiletype
 
+
 T_BaseArchive = TypeVar("BaseArchive")
+
+
+@attr.s
+class ArchiveFile(object):
+    """An generic archive file object.
+    """
+
+    filepath = attr.ib(type=str, converter=Path)
+    data = attr.ib(type=bytes, repr=False)
+
+    @property
+    def size(self) -> int:
+        """int: The full size of the file.
+        """
+        return len(self.data)
 
 
 @attr.s
@@ -19,7 +36,7 @@ class BaseArchive(BaseFiletype, abc.ABC, Generic[T_BaseArchive]):
     """The base class all Archives should subclass.
     """
     content = attr.ib(type=bytes, repr=False)
-    filepath = attr.ib(type=str, default=None)
+    filepath = attr.ib(type=str, default=None, converter=Path)
     container = attr.ib(type=Container, default=None, repr=False, init=False)
 
     def __attrs_post_init__(self):
@@ -96,24 +113,59 @@ class BaseArchive(BaseFiletype, abc.ABC, Generic[T_BaseArchive]):
             T_BaseArchive: A created `BaseArchive`
         """
         if not os.path.isfile(filepath):
-            raise FileNotFoundError(f"no such file '{filepath!r}' exists")
+            raise FileNotFoundError(f"no such file {filepath!r} exists")
 
         with open(filepath, "rb") as stream:
             return cls.parse_stream(stream, filepath)
 
     @abc.abstractmethod
+    def iter_files(self) -> Generator[ArchiveFile, None, None]:
+        """Iterates over the available files in the archive.
+
+        Yields:
+            ArchiveFile: An archive file
+
+        Raises:
+            NotImplementedError: Subclasses must implement
+        """
+        raise NotImplementedError
+
     def extract(
         self, to_dir: str, progress_hook: Callable[[int, int, str], None] = None
     ):
         """Extracts the content of the `BaseArchive` to the given directory.
+
+        Note:
+            The provided progress hook is simple and two-stage. It is called once
+            before a file is being written and once after the same file is done
+            being written.
 
         Args:
             to_dir (str): The directory to extract the content to
             progress_hook (Callable[[int, int, str], None], optional): Defaults to None.
                 A progress hook that should expect (``current``, ``total``,
                 ``current_filepath``) as arguments
-
-        Raises:
-            NotImplementedError: Subclasses must implement
         """
-        raise NotImplementedError
+
+        if not os.path.isdir(to_dir):
+            raise NotADirectoryError(f"no directory {to_dir!r} exists")
+
+        to_dir = Path(to_dir)
+
+        archive_files = list(self.iter_files())
+        total_size = sum(entry.size for entry in archive_files)
+        current_size = 0
+
+        for entry in archive_files:
+            to_path = to_dir.joinpath(entry.filepath)
+            if callable(progress_hook):
+                progress_hook(current_size, total_size, to_path.as_posix())
+
+            if not to_path.parent.is_dir():
+                to_path.parent.mkdir(parents=True)
+            with to_path.open("wb") as stream:
+                stream.write(entry.data)
+            current_size += entry.size
+
+            if callable(progress_hook):
+                progress_hook(current_size, total_size, to_path.as_posix())
