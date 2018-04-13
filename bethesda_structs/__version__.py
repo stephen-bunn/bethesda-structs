4eb9811ed8c3e800296aec2f7333974863f86542
Stephen Bunn <stephen@bunn.io>
2018-04-13 09:50:44 -0400

    Updating copyright years
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- bethesda_structs/__version__.py
+++ bethesda_structs/__version__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Stephen Bunn (stephen@bunn.io)
+# Copyright (c) 2018 Stephen Bunn (stephen@bunn.io)
 # GPLv3 License <https://choosealicense.com/licenses/gpl-3.0/>
 
 __name__ = "bethesda-structs"
